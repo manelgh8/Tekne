f968d35da1d320fc910e31daba3649a5d283f595
manel ghazi <108812624+manelgh8@users.noreply.github.com>
2025-01-22 23:07:19 +0100

    Update Navbar.tsx

--- project/src/components/Navbar.tsx
+++ project/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import logo from '../assets/logt.png';
+import logo from '../assets/Logt.png';
 
 const Navbar = () => {
   return (
